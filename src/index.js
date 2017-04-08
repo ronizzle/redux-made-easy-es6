ef8921d3a829cba11d46bfb821050fa99914e95a
ronizzle <roniel44@gmail.com>
2017-04-08 09:32:15 +0800

    ES6 implementation of the counter project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-console.log('hello')
+import { createStore, combineReducers} from 'redux'
+/**
+ * this is the reducer function
+ */
+function counter(state={count:0}, action) {
+
+    if(typeof state === 'undefined') {
+        return { count: 0 }
+    }
+
+    var nextState = {
+        count: state.count
+    }
+    switch(action.type) {
+        case 'ADD':
+            nextState.count = state.count + 1
+            return nextState
+            break
+        case 'MINUS':
+            nextState.count = state.count - 1
+            return nextState
+            break
+        case 'RESET':
+            nextState.count = 0
+            return nextState
+            break
+        default:
+            return nextState
+            break
+    }
+}
+
+function render() {
+    var state = store.getState()
+    counterEl.innerHTML = state.count.toString()
+}
+
+// STORE
+const state = {count: 0},
+    store = createStore(counter),
+    counterEl = document.getElementById('counter')
+
+store.subscribe(render)
+
+document.getElementById('add').addEventListener('click',() => {
+  store.dispatch({type: 'ADD'})
+})
+
+
+document.getElementById('minus').addEventListener('click',() => {
+  store.dispatch({type: 'MINUS'})
+})
+
+
+document.getElementById('reset').addEventListener('click', () => {
+
+          store.dispatch({type: 'RESET'})
+})
+
+render()
